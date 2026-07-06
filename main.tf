82888d9aefca2d0401d401105b053e11adba0308
pleontis <panagiotisleontis@gmail.com>
2026-07-06 22:55:01 +0300

    Remediate Azure Storage vulnerabilities: enforce HTTPS, TLS 1.2 and disable public access

--- main.tf
+++ main.tf
@@ -3,23 +3,23 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "vulnerable-rg"
+  name     = "secure-rg"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "vulnerable_storage" {
-  name                     = "vulnstorageacct12345"
+resource "azurerm_storage_account" "secure_storage" {
+  name                     = "securestorageacct12345"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # VULNERABILITY 1: Allows HTTP traffic (Secure transfer disabled)
-  enable_https_traffic_only = false
+  # REMEDIATION 1: Enforce HTTPS only (Secure transfer required)
+  enable_https_traffic_only = true
 
-  # VULNERABILITY 2: Allows outdated and vulnerable TLS 1.0
-  min_tls_version = "TLS1_0"
+  # REMEDIATION 2: Enforce modern, secure TLS 1.2
+  min_tls_version = "TLS1_2"
 
-  # VULNERABILITY 3: Allows public network access to the storage account
-  public_network_access_enabled = true
+  # REMEDIATION 3: Block public network access
+  public_network_access_enabled = false
 }
